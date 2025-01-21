0c2631b9bb185954f276c579d7ca0e8ab9ec48d5
P Jaworski <pjaworski.dev@gmail.com>
2025-01-21 01:54:01 +0100

    Bugfix: background.js ex. imports

--- src/assets/scripts/background.js
+++ src/assets/scripts/background.js
@@ -12,11 +12,15 @@
 //   - You should have received a copy of the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) License
 //   - along with Memento Extension.  If not, see <https://creativecommons.org/licenses/by-nc-nd/4.0/>.
 
-import { browser_cr, getIs12HourFormat } from "./functionality/tools";
 
-const WELCOME_URL = "https://weblxapplications.com/sp/welcome";
-const UDPDATE_URL = "https://weblxapplications.com/sp/update";
-const DELETE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfy3DYPCFQniaGrZjtYnlSUxQyZeBNFicYk9aMJwu6FbS86lg/viewform?usp=header";
+function getIs12HourFormat() {
+  let is12HourFormat = false; // Default fallback
+  try {
+      is12HourFormat = new Date().toLocaleString('en', { hour: '2-digit', hour12: true }).indexOf(':') !== -1;
+  } catch (error) {
+      console.warn('Error determining time format:', error);
+  }
+}
 
 const initialState = {
   disabled: false,
@@ -27,7 +31,7 @@ const initialState = {
   time__show_seconds: true,
   time__is_12_hours: getIs12HourFormat(), 
 
-  date__isvisible: false,
+  date__isvisible: true,
 
   background_blur: 5,
   background_brightness: 0.22,
@@ -41,8 +45,11 @@ const initialState = {
 };
 
 
+const browser_cr = chrome ? chrome : browser;
+
 function initStateIfNotExist() {
   browser_cr.storage?.local?.get("formState", (result) => {
+    console.log(result, result?.formState);
     
     if (!result?.formState || Object.keys(result.formState).length < 4) {
       if (typeof window !== "undefined" && window.location) {
@@ -63,14 +70,14 @@ browser_cr.runtime.onInstalled.addListener(function (details) {
   if (details.reason === 'install' || details.reason === 'update') {
     chrome.storage.local.get('welcomePageDisplayed', function (data) {
       if (!data.welcomePageDisplayed && details.reason === 'install') {
-        // chrome.tabs.create({ url: WELCOME_URL });
+        // chrome.tabs.create({ url: "https://weblxapplications.com/sp/welcome" });
         chrome.storage.local.set({ 'welcomePageDisplayed': true });
       } else if (details.reason === 'update') {
-        // chrome.tabs.create({ url: UDPDATE_URL });
+        // chrome.tabs.create({ url: "https://weblxapplications.com/sp/update" });
       }
     });
   }
 });
 
 
-browser_cr.runtime.setUninstallURL(DELETE_URL);
+browser_cr.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSfy3DYPCFQniaGrZjtYnlSUxQyZeBNFicYk9aMJwu6FbS86lg/viewform?usp=header");
